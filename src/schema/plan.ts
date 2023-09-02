75f6f63298d8f7fa7a5c5361bb09d812a88e3932
VorakornK <vorakorn.ko@ku.th>
2023-09-02 22:19:31 +0700

    add testcase

--- src/schema/plan.ts
+++ src/schema/plan.ts
@@ -1,22 +1,22 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const createSchema = z.object({
-    project_id: z.string(),
-    name: z.string(),
-    description: z.string().optional(),
-    start_date: z.string(),
-    end_date: z.string(),
-    task: z.boolean(),
-  })
+  project_id: z.string(),
+  name: z.string(),
+  description: z.string().optional(),
+  start_date: z.string(),
+  end_date: z.string(),
+  task: z.boolean(),
+})
 
 export const editSchema = z.object({
-    _id: z.string(),
-    name: z.string().optional(),
-    description: z.string().optional(),
-    progress: z.number(),
-    start_date: z.string(),
-    end_date: z.string(),
-  })
+  _id: z.string(),
+  name: z.string().optional(),
+  description: z.string().optional(),
+  progress: z.number(),
+  start_date: z.string(),
+  end_date: z.string(),
+})
 
 export type createSchema = z.infer<typeof createSchema>
 export type editSchema = z.infer<typeof editSchema>
--- src/schema/summary.ts
+++ src/schema/summary.ts
@@ -1,14 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 export const createSchema = z.object({
-    project_id: z.string(),
-    plan_id: z.string(),
-    reciever_id: z.string(),
-    sender_id: z.string(),
-    comment: z.string(),
-    file_id: z.array(z.string()),
-    chat_id: z.string(),
-    progress: z.number(),
-    })
+  project_id: z.string(),
+  plan_id: z.string(),
+  reciever_id: z.string(),
+  sender_id: z.string(),
+  comment: z.string(),
+  file_id: z.array(z.string()),
+  chat_id: z.string(),
+  progress: z.number(),
+})
 
 export type createSchema = z.infer<typeof createSchema>
