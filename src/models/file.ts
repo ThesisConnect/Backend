6180c4c174f44dc66edfaf6efc91d466ee29855d
Pilleteer <patcharapol.ra@ku.th>
2023-09-02 22:45:38 +0700

    add file model

---
+++ src/models/file.ts
@@ -0,0 +1,50 @@
+import {
+    Document,
+    Model,
+    model,
+    Schema,
+    SchemaTimestampsConfig,
+  } from 'mongoose'
+  import { uuidv4 } from '@firebase/util'
+  
+  export interface IFile {
+    _id: string
+    name: string
+    url: string
+    size: number
+    type: string
+  }
+  
+  interface IFileDocument extends IFile, Document, SchemaTimestampsConfig {
+    _id: string
+  }
+  
+  type IFileModel = Model<IFileDocument>
+  const fileSchema = new Schema<IFileDocument, IFileDocument>(
+    {
+        _id: {
+            type: String,
+            default: uuidv4(),
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+        size: {
+            type: Number,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true },
+  )
+  
+  export default model<IFileDocument, IFileModel>('File', fileSchema)
+  
