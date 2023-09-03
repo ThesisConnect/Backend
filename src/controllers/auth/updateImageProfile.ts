88d6c3c2fe804410274ebe63c49b9a1b8ec07b2a
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-03 13:35:42 +0700

    fix updateProfile

--- src/controllers/auth/updateImageProfile.ts
+++ src/controllers/auth/updateImageProfile.ts
@@ -7,11 +7,13 @@ import deleteFile from '../services/deleteFile';
 
 const updateImageProfileUrl =async (req: Request, res: Response) => {
     try {
-        const oldpath = req.body.oldpath;
         const url = await generateSignedUrl("imageProfile",'my_profile');
-        const oldDirectory = oldpath.split("/")[3];
-        const oldFileName = oldpath.split("/")[4];
-        await deleteFile(oldDirectory,oldFileName);
+        if(req.body.oldpath){
+            const oldpath = req.body.oldpath;
+            const oldDirectory = oldpath.split("/")[3];
+            const oldFileName = oldpath.split("/")[4];
+            await deleteFile(oldDirectory,oldFileName);
+        }
         res.json({ url });
       } catch (error) {
         res.status(500).send('Error generating signed URL.');
