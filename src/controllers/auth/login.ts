ebd234d8ea8d0ee4e102e60d0aa6cff1a2d0b17e
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-21 12:25:15 +0700

    fix bug

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -12,28 +12,32 @@ const login = async (req: Request, res: Response) => {
   //     res.status(401).send('UNAUTHORIZED REQUEST!');
   //     return;
   //   }
+  if (!idToken) {
+    return res.status(400).send('ID token is missing');
+  }
   const expiresIn = ms('2d')
 
   try {
-    const sessionCookie = await admin
-      .auth()
-      .createSessionCookie(idToken, { expiresIn })
-    let options: object = { maxAge: expiresIn, httpOnly: true, secure: false }
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    const { uid, email } = decodedIdToken;
+
+    // Check if user exists in your database
+    const user = await User.findById(uid);
+    if (!user) {
+      return res.status(404).send('User not found in the database');
+    }
+    const sessionCookie = await admin.auth().createSessionCookie(idToken, { expiresIn });
+    let options: any = { maxAge: expiresIn, httpOnly: true, secure: false };
     if (!isDev) {
       options = {
         ...options,
         secure: true,
         sameSite: 'none',
         domain: 'railway.app',
-      }
+      };
     }
-    res.cookie('session', sessionCookie, options)
-    const decodedIdToken = await admin.auth().verifyIdToken(idToken)
-    const { uid, email } = decodedIdToken
-    const user = await User.findById(uid)
-    if (!user) {
-      throw new Error('User not found!')
-    }
+    
+    res.cookie('session', sessionCookie, options);
     const sentData = { ...user.toObject(), email, isAuthenticated: true }
     return res.status(200).send(_.omit(sentData, ['_id', '__v']))
   } catch (error) {
