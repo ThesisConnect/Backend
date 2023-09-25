474afc8351acd294d345cfd53c768ab07ae9b9dd
VorakornK <vorakorn.ko@ku.th>
2023-09-25 12:24:36 +0700

    request-by-elle

--- src/schema/plan.ts
+++ src/schema/plan.ts
@@ -14,8 +14,8 @@ export const editSchema = z.object({
   name: z.string().optional(),
   description: z.string().optional(),
   progress: z.number(),
-  start_date: z.string(),
-  end_date: z.string(),
+  start_date: z.string().optional(),
+  end_date: z.string().optional(),
 })
 
 export type createSchema = z.infer<typeof createSchema>
