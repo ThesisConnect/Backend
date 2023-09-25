3c9a8abde7f0ffbe125343960bafe30c93680ac1
Pilleteer <patcharapol.ra@ku.th>
2023-09-25 10:05:59 +0700

    can add file in edit summary

--- src/routers/summary.ts
+++ src/routers/summary.ts
@@ -179,6 +179,7 @@ router.put('/edit', async (req, res) => {
       comment: editData.data.comment,
       status: editData.data.status,
       progress: editData.data.progress,
+      $addToSet: { files: editData.data.files },
     })
 
     if (result) {
