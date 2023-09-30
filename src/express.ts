0354d6a16472808814b4ae34a6f0d1f7a3575060
Paxwell-Paxwell <pompom231245@gmail.com>
2023-09-30 22:01:03 +0700

    add test Api

--- src/express.ts
+++ src/express.ts
@@ -79,6 +79,7 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/auth', authRoute)
 app.use('/test', jwtMiddleware, router)
 app.use('/', jwtMiddleware,router)
+app.use('/testApi',router)
 //next()
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (res.headersSent) {
